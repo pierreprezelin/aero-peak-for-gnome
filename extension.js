5443ee6080ad82abad8a46b52588ba07f25c2972
Pierre Prezelin <pierreprezelin.pro@gmail.com>
2026-06-29 11:38:11 +0200

    feat: add minimize and maximize behaviour

--- extension.js
+++ extension.js
@@ -1,37 +1,56 @@
-import St from 'gi://St';
 import Clutter from 'gi://Clutter';
+import GObject from 'gi://GObject';
+import Meta from 'gi://Meta';
+import St from 'gi://St';
 
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
+const AeroPeakButton = GObject.registerClass(
+    class AeroPeakButton extends PanelMenu.Button {
+        _init(extension) {
+            super._init(0.0, extension.metadata.name, true);
+            this._extension = extension;
+
+            const icon = new St.Icon({
+                icon_name: 'face-laugh-symbolic',
+                style_class: 'system-status-icon',
+            });
+            this.add_child(icon);
+        }
+
+        vfunc_event(event) {
+            if (event.type() === Clutter.EventType.BUTTON_PRESS) {
+                this._extension.toggleWindows();
+                return Clutter.EVENT_STOP;
+            }
+            return Clutter.EVENT_PROPAGATE;
+        }
+    }
+);
+
 export default class AeroPeakForWindows extends Extension {
     enable() {
-        log('Aero Peak: Enable called');
-        // Create a panel button
-        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
-
-        // Add an icon
-        const icon = new St.Icon({
-            icon_name: 'face-laugh-symbolic',
-            style_class: 'system-status-icon',
-        });
-        this._indicator.add_child(icon);
-
-        // Connect click behaviour
-        this._indicator.connect('button-press-event', () => {
-            log('clicked');
-            return Clutter.EVENT_PROPAGATE;
-        });
-        log('Aero Peak: Event connected');
-
-        // Add the indicator to the panel
+        this._indicator = new AeroPeakButton(this);
         Main.panel.addToStatusArea(this.uuid, this._indicator);
     }
 
     disable() {
-        log('Aero Peak: Disabled called');
         this._indicator?.destroy();
         this._indicator = null;
+    }
+
+    toggleWindows() {
+        const workspace = global.workspace_manager.get_active_workspace();
+        const windows = workspace.list_windows();
+
+        const hasVisibleWindows = windows.some(w => !w.minimized);
+
+        if (hasVisibleWindows) {
+            windows.forEach(w => w.minimize());
+        } else {
+            windows.forEach(w => w.unminimize());
+        }
     }
 }
