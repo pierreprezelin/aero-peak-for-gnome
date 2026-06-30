4dc29d8d4f29256422bf894b5add7f0382d90862
Pierre Prezelin <pierreprezelin.pro@gmail.com>
2026-06-30 10:03:12 +0200

    fix: add animation delay only on mouse-enter

--- extension.js
+++ extension.js
@@ -150,17 +150,18 @@ export default class AeroPeakForWindows extends Extension {
 
         windows.forEach(w => {
             if (w.minimized) return;
-            if (!this._settings.get_boolean('peak-on-hover')) return;
+            if (enable && !this._settings.get_boolean('peak-on-hover')) return;
             if (this._shouldIgnore(w)) return;
 
             const actor = w.get_compositor_private();
             if (actor) {
+                actor.remove_all_transitions(); // Cancel any ongoing animation to avoid conflicts
                 actor.ease({
                     opacity: enable
                         ? (this._settings.get_int('peak-opacity') / 100) * 255
                         : 255,
                     duration: this._settings.get_int('peak-duration'),
-                    delay: this._settings.get_int('peak-delay'),
+                    delay: enable ? this._settings.get_int('peak-delay') : 0,
                     mode: Clutter.AnimationMode.EASE_OUT_QUAD,
                 });
             }
